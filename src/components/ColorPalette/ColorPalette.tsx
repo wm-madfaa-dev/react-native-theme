948e8e0755ffd461f88322f79fd2d9a86c352dc9
Wasim Almadfaa <wm.madfaa.dev@gmail.com>
2019-12-23 11:28:16 +0100

    add color palette screen

---
+++ src/components/ColorPalette/ColorPalette.tsx
@@ -0,0 +1,142 @@
+import React from "react";
+import { View, Text, StyleSheet, ViewStyle } from "react-native";
+
+export interface ColorPaletteItemProps {
+  label: string;
+  color: ViewStyle["backgroundColor"];
+  centeredLabel?: boolean;
+  type?: "light" | "dark";
+}
+
+export interface ColorPaletteProps {
+  title: string;
+  colors: ColorPaletteItemProps[];
+}
+
+export interface Palette {
+  main: ColorPaletteItemProps;
+  light: ColorPaletteItemProps;
+  dark: ColorPaletteItemProps;
+}
+
+export interface ThemeColorPaletteProps {
+  title: string;
+  palette: Palette;
+  colors: ColorPaletteItemProps[];
+}
+
+const ColorPaletteItem: React.FC<ColorPaletteItemProps> = ({
+  label,
+  color,
+  centeredLabel,
+  type
+}) => {
+  return (
+    <View style={styles.colorRoot}>
+      <View style={styles.colorBorder}>
+        <View style={[styles.color, { backgroundColor: color }]} />
+      </View>
+      <Text
+        style={[
+          styles.label,
+          centeredLabel ? styles.centeredLabel : {},
+          type === "dark" ? styles.labelWhite : {}
+        ]}
+      >
+        {label}
+      </Text>
+    </View>
+  );
+};
+
+const ColorPalette: React.FC<ColorPaletteProps> = ({ title, colors }) => {
+  return (
+    <View style={styles.root}>
+      <Text style={styles.title}>{title}</Text>
+      <View style={styles.colors}>
+        {colors.map((color, index) => (
+          <ColorPaletteItem key={index} {...color} />
+        ))}
+      </View>
+    </View>
+  );
+};
+
+export const ThemeColorPalette: React.FC<ThemeColorPaletteProps> = ({
+  title,
+  palette,
+  colors
+}) => {
+  return (
+    <View style={styles.root}>
+      <Text style={styles.title}>{title}</Text>
+      <View style={styles.colors}>
+        <ColorPaletteItem label="Main" color={palette.main.color} />
+        <ColorPaletteItem label="Light" color={palette.light.color} />
+        <ColorPaletteItem label="Dark" color={palette.dark.color} />
+      </View>
+      <View style={styles.colors}>
+        <ColorPaletteItem centeredLabel {...palette.main} />
+        {colors.map(
+          (color, index) =>
+            color.color !== palette.main.color && (
+              <ColorPaletteItem centeredLabel key={index} {...color} />
+            )
+        )}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    flexDirection: "column",
+    paddingVertical: 15
+  },
+  title: {
+    fontStyle: "normal",
+    fontWeight: "bold",
+    color: "rgba(0, 0, 0, 0.87)",
+    fontSize: 15,
+    lineHeight: 24,
+    marginHorizontal: 15
+  },
+  colors: {
+    flexDirection: "row",
+    flexWrap: "wrap"
+  },
+  colorRoot: {
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 10,
+    marginHorizontal: 10
+  },
+  colorBorder: {
+    padding: 5,
+    borderWidth: 1,
+    borderColor: "#202020",
+    marginBottom: 5,
+    borderRadius: 50
+  },
+  color: {
+    width: 45,
+    height: 45,
+    borderRadius: 50
+  },
+  label: {
+    fontStyle: "normal",
+    fontWeight: "normal",
+    color: "rgba(0, 0, 0, 0.87)",
+    fontSize: 12,
+    lineHeight: 16
+  },
+  labelWhite: {
+    color: "#FFFFFF"
+  },
+  centeredLabel: {
+    position: "absolute"
+  }
+});
+
+export default ColorPalette;
--- src/screens/ColorPalette/ColorPalette.screen.tsx
+++ src/screens/ColorPalette/ColorPalette.screen.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { ScrollView, StyleSheet } from "react-native";
 import { RouteProp } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParams, ROUTES } from "../../app/app";
+import ColorPalette from "../../components/ColorPalette/ColorPalette";
+import { ThemeColorPalette } from "../../components/ColorPalette/ColorPalette";
 
 type ColorPaletteScreenNavigationProp = StackNavigationProp<
   RootStackParams,
@@ -20,14 +22,92 @@ interface ColorPaletteScreenProps {
 
 const ColorPaletteScreen: React.FC<ColorPaletteScreenProps> = () => {
   return (
-    <View style={styles.root}>
-      <Text>ColorPalette Screen</Text>
-    </View>
+    <ScrollView style={styles.root}>
+      <ColorPalette
+        title="Background"
+        colors={[
+          { label: "Light", color: "#FAFAFA" },
+          { label: "Dark", color: "#E8E8E8" }
+        ]}
+      />
+      <ColorPalette
+        title="White overlay"
+        colors={[
+          { label: "Hover", color: "rgba(255, 255, 255, 0.12)" },
+          { label: "Focus", color: "rgba(255, 255, 255, 0.08)" },
+          { label: "Pressed", color: "rgba(255, 255, 255, 0.32)" }
+        ]}
+      />
+      <ColorPalette
+        title="Black overlay"
+        colors={[
+          { label: "Hover", color: "#000000" },
+          { label: "Focus", color: "rgba(0, 0, 0, 0.12)" },
+          { label: "Pressed", color: "rgba(0, 0, 0, 0.16)" }
+        ]}
+      />
+      <ColorPalette
+        title="Surface"
+        colors={[
+          { label: "App Ui", color: "#FFFFFF" },
+          { label: "Card", color: "#FAFAFA" },
+          { label: "Dialogs", color: "#FFFFFF" },
+          { label: "Drawers", color: "#FFFFFF" },
+          { label: "Sheets", color: "#FAFAFA" },
+          { label: "Side Sheets", color: "#FFFFFF" },
+          { label: "Menu", color: "#FAFAFA" },
+          { label: "Snackbar", color: "#202020" }
+        ]}
+      />
+      <ThemeColorPalette
+        title="Primary"
+        palette={{
+          main: { label: "500", color: "#6202EE", type: "dark" },
+          light: { label: "50", color: "#F2E7FE", type: "light" },
+          dark: { label: "900", color: "#190078", type: "dark" }
+        }}
+        colors={[
+          { label: "50", color: "#F2E7FE" },
+          { label: "100", color: "#D7B7FD" },
+          { label: "200", color: "#D7B7FD" },
+          { label: "300", color: "#BB86FC" },
+          { label: "400", color: "#7F22FD", type: "dark" },
+          { label: "500", color: "#6202EE", type: "dark" },
+          { label: "600", color: "#4B01D1", type: "dark" },
+          { label: "700", color: "#3700B3", type: "dark" },
+          { label: "800", color: "#280096", type: "dark" },
+          { label: "900", color: "#190078", type: "dark" }
+        ]}
+      />
+      <ThemeColorPalette
+        title="Secondary"
+        palette={{
+          main: { label: "500", color: "#01A39D", type: "dark" },
+          light: { label: "50", color: "#86FFEA", type: "light" },
+          dark: { label: "900", color: "#002F33", type: "dark" }
+        }}
+        colors={[
+          { label: "50", color: "#86FFEA" },
+          { label: "100", color: "#18FDE1" },
+          { label: "200", color: "#18FDE1" },
+          { label: "300", color: "#03DAC6" },
+          { label: "400", color: "#02BEB2", type: "dark" },
+          { label: "500", color: "#01A39D", type: "dark" },
+          { label: "600", color: "#018786", type: "dark" },
+          { label: "700", color: "#00696B", type: "dark" },
+          { label: "800", color: "#004B4F", type: "dark" },
+          { label: "900", color: "#002F33", type: "dark" }
+        ]}
+      />
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  root: { flex: 1, alignItems: "center", justifyContent: "center" }
+  root: {
+    flex: 1,
+    backgroundColor: "#FAFAFA"
+  }
 });
 
 export default ColorPaletteScreen;
