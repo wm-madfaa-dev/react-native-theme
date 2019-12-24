d679d221dfe0707f41e1487e1d5f4f58158ed1e9
Wasim Almadfaa <wm.madfaa.dev@gmail.com>
2019-12-24 19:56:15 +0100

    add createSpacing

--- src/styles/themeManager/createTheme.ts
+++ src/styles/themeManager/createTheme.ts
@@ -1,17 +1,28 @@
 import deepmerge from "deepmerge";
 import createPalette, { PaletteOptions, Palette } from "../createColorPalette";
+import createTypography, {
+  TypographyOptions,
+  Typography
+} from "../createTypography";
 
 export interface ThemeOptions {
   palette?: PaletteOptions;
+  typography?: TypographyOptions;
 }
 export interface Theme {
   palette: Palette;
+  typography: Typography;
 }
 
 const createTheme = (options: ThemeOptions): Theme => {
-  const { palette: paletteInput = {}, ...others } = options;
+  const {
+    palette: paletteInput = {},
+    typography: typographyInput = {},
+    ...others
+  } = options;
   const palette = createPalette(paletteInput);
-  return deepmerge({ palette }, others);
+  const typography = createTypography(typographyInput);
+  return deepmerge({ palette, typography }, others);
 };
 
 export default createTheme;
