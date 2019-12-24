b48080ed45088827e579a1c6e8765f2f608b9a63
Wasim Almadfaa <wm.madfaa.dev@gmail.com>
2019-12-24 21:56:26 +0100

    add shadows

--- src/styles/themeManager/createTheme.ts
+++ src/styles/themeManager/createTheme.ts
@@ -4,25 +4,41 @@ import createTypography, {
   TypographyOptions,
   Typography
 } from "../createTypography";
+import createSpacing, { SpacingOptions, Spacing } from "../createSpacing";
+import defaultShadows, { createShadowArgs, Shadows } from "../shadows";
+import { createShadow } from "../shadows";
 
 export interface ThemeOptions {
   palette?: PaletteOptions;
   typography?: TypographyOptions;
+  spacing?: SpacingOptions;
+  shadows?: { [key: string]: createShadowArgs };
 }
 export interface Theme {
   palette: Palette;
   typography: Typography;
+  spacing: Spacing;
+  shadows: Shadows;
 }
 
 const createTheme = (options: ThemeOptions): Theme => {
   const {
     palette: paletteInput = {},
     typography: typographyInput = {},
+    spacing: spacingInput,
+    shadows: shadowsInput = {},
     ...others
   } = options;
   const palette = createPalette(paletteInput);
   const typography = createTypography(typographyInput);
-  return deepmerge({ palette, typography }, others);
+  const spacing = createSpacing(spacingInput);
+
+  const shadows = Object.keys(shadowsInput).reduce((acc, shadowKey) => {
+    const { depth, blur, color } = shadowsInput[shadowKey];
+    return { ...acc, [shadowKey]: createShadow(depth, blur, color) };
+  }, defaultShadows);
+
+  return deepmerge({ palette, typography, spacing, shadows }, others);
 };
 
 export default createTheme;
